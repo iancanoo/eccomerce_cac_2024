c6539be4dbe0fb5662a44051a6e83347cff326c5
iancano <iancarp20022017@gmail.com>
2024-06-29 16:02:07 -0300

    conexion_DB

--- DB/db.js
+++ DB/db.js
@@ -1,5 +1,13 @@
 import { createPool } from "mysql2/promise";
 
+//Conexion con pool
+// const pool = createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_DATABASE,
+//   connectionLimit: 5,
+// });
 const pool = createPool({
   host: "localhost",
   user: "root",
@@ -7,3 +15,19 @@ const pool = createPool({
   database: "tienda",
   connectionLimit: 5,
 });
+//Verificacion de conexion
+pool
+  .getConnection()
+  .then((connection) => {
+    console.log("Connected to the database");
+    connection.release();
+  })
+  .catch((error) => {
+    console.log("Error connect to the database", error);
+    console.log("DB_HOST:", process.env.DB_HOST);
+    console.log("DB_USER:", process.env.DB_USER);
+    console.log("DB_PASS:", process.env.DB_PASS);
+    console.log("DB_DATABASE:", process.env.DB_DATABASE);
+  });
+
+export default pool;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { register, login } from "../controllers/authController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
+import conexion from "../DB/db.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/", (req, res) => {
+  conexion();
   res.render("index");
 });
 
